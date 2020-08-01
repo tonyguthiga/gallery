a0880c7849801a564d47f83d42e958273c38db87
antony-guthiga <toniguthiga93@gmail.com>
2020-08-01 17:37:24 +0300

    add test for category to test instance and save method

--- gallery/tests.py
+++ gallery/tests.py
@@ -1,3 +1,18 @@
 from django.test import TestCase
+from .models import Category,Image,Location
 
 # Create your tests here.
+class CategoryTestClass(TestCase):
+    # Set up method
+    def setUp(self):
+        self.travel= Category(name = 'Travel')
+
+    # Testing instance
+    def test_instance(self):
+        self.assertTrue(isinstance(self.travel,Category))
+
+    # Testing Save Method
+    def test_save_method(self):
+        self.travel.save_category()
+        categories = Category.objects.all()
+        self.assertTrue(len(categories) > 0)
