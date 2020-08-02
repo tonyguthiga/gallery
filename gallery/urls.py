33bd68a1a12fa07c8bc54d964c54ad1a95c751c1
antony-guthiga <toniguthiga93@gmail.com>
2020-08-02 21:44:50 +0300

    add url paths

--- gallery/urls.py
+++ gallery/urls.py
@@ -1,6 +1,13 @@
-from django.urls import path
 from . import views
+from django.conf.urls import url
+from django.conf import settings
+from django.conf.urls.static import static
+from django.urls import path
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    url(r'^$', views.index, name='index'),
+    url(r'^search/', views.search_image, name='search_results'),
+    path(r'image/<int:pk>/',views.detail,name = 'detail')
 ]
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
