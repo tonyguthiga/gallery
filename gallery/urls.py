72960aaa0498d3de18690d83d824d79271867306
antony-guthiga <toniguthiga93@gmail.com>
2020-08-03 21:27:05 +0300

    add detail url path

--- gallery/urls.py
+++ gallery/urls.py
@@ -7,7 +7,7 @@ from django.urls import path
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^search/', views.search_image, name='search_results'),
-    path(r'image/<int:pk>/',views.detail,name = 'detail')
+    path(r'image/<int:pk>/',views.detail,name = 'detail'),
 ]
 if settings.DEBUG:
     urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
